831436ce0f26fc4f70509b540d1ccb3416e28973
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2015-08-04 18:26:37 -0300

    improve package installation

--- setup.bash
+++ setup.bash
@@ -2,8 +2,13 @@
 
 echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" | sudo tee -a /etc/apt/sources.list
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+
 sudo apt-get update
-sudo apt-get install -y --force-yes linux-headers-$(uname -r) build-essential virtualbox-4.3 dkms
+sudo apt-get install -y --force-yes linux-headers-$(uname -r)
+sudo apt-get install -y --force-yes build-essential
+sudo apt-get install -y --force-yes virtualbox-4.3
+sudo apt-get install -y --force-yes dkms
+
 wget http://download.virtualbox.org/virtualbox/4.3.10/Oracle_VM_VirtualBox_Extension_Pack-4.3.10-93012.vbox-extpack
 sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-4.3.10-93012.vbox-extpack
 

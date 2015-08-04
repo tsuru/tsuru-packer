f1807c5fde973721135d43da293368778b8745b6
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2015-08-04 16:07:11 -0300

    add license

--- run.rc.local.bash
+++ run.rc.local.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright 2015 tsuru authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 RC_LOCAL_FILE=$(cat <<'EOF'
 #!/bin/bash -ue
 

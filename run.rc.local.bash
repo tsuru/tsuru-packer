2a19c70ae0d51636e9029cbf3690f371bac672e3
Paulo Sousa <paulo.sousa@corp.globo.com>
2015-08-12 13:50:30 -0300

    rc.local: fix replace for private ips

--- run.rc.local.bash
+++ run.rc.local.bash
@@ -47,38 +47,62 @@ function public_ip {
     echo "${ip}"
 }
 
-old_ip=$(cat /etc/tsuru/tsuru.conf | grep ^host | sed "s/host: http:\/\///" | sed "s/:8080//")
-if [[ "$old_ip" == "" ]]; then
-    error "Couldn't find old_ip"
+function aws_local_ip {
+    # Try to take the public IP using AWS EC2's metadata API:
+    local ip=$(curl -s -L -m2 http://169.254.169.254/latest/meta-data/local-ipv4 || true)
+    echo "${ip}"
+}
+
+function private_ip {
+    local private_ip=$(aws_local_ip)
+    if [[ $private_ip == "" ]]; then
+        private_ip=$(public_ip)
+    fi
+    if [[ $private_ip == "127.0.0.1" ]]; then
+        echo "Couldn't find suitable local_ip, please run with --host-ip <external ip>"
+        exit 1
+    fi
+    echo "${private_ip}"
+}
+
+
+
+old_public_ip=$(cat /etc/tsuru/tsuru.conf | grep ^host | sed "s/host: http:\/\///" | sed "s/:8080//")
+old_private_ip=$(cat ~ubuntu/.tsuru_target | awk -F : '{print $1}')
+
+if [[ "$old_public_ip" == "" || "$old_private_ip" == "" ]]; then
+    error "Couldn't find old private or public ip"
     exit 1
 fi
-new_ip=$(public_ip)
-sed -i "s/$old_ip/$new_ip/g" /etc/tsuru/tsuru.conf
+new_public_ip=$(public_ip)
+new_private_ip=$(private_ip)
+
+sed -i "s/$old_public_ip/$new_public_ip/g" /etc/tsuru/tsuru.conf
 if [[ $? -gt 0 ]]; then
     error "Cannot change public ip in /etc/tsuru/tsuru.conf, please do it manually."
     exit 1
 fi
-sed -i "s/$old_ip/$new_ip/g" /etc/gandalf.conf
+sed -i "s/$old_public_ip/$new_public_ip/g" /etc/gandalf.conf
 if [[ $? -gt 0 ]]; then
     error "Cannot change public ip in /etc/gandalf.conf, please do it manually."
     exit 1
 fi
-sed -i "s/$old_ip/$new_ip/g" ~/.tsuru_target
+sed -i "s/$old_private_ip/$new_private_ip/g" ~ubuntu/.tsuru_target
 if [[ $? -gt 0 ]]; then
-    error "Cannot change public ip in ~/.tsuru_target, please do it manually."
+    error "Cannot change private ip in ~ubuntu/.tsuru_target, please do it manually."
     exit 1
 fi
-sed -i "s/$old_ip/$new_ip/g" ~git/.bash_profile
+sed -i "s/$old_private_ip/$new_private_ip/g" ~ubuntu/.tsuru_targets
 if [[ $? -gt 0 ]]; then
-    error "Cannot change public ip in ~git/.bash_profile, please do it manually."
+    error "Cannot change private ip in ~ubuntu/.tsuru_targets, please do it manually."
     exit 1
 fi
-local_ip=$(curl -s -L -m2 http://169.254.169.254/latest/meta-data/local-ipv4)
-sed -i "s/${new_ip}:6060/${local_ip}:6060/g" ~git/.bash_profile
+sed -i "s/$old_private_ip/$new_private_ip/g" ~git/.bash_profile
 if [[ $? -gt 0 ]]; then
-    error "Cannot change local ip in ~git/.bash_profile, please do it manually."
+    error "Cannot change private ip in ~git/.bash_profile, please do it manually."
     exit 1
 fi
+
 EOF
 )
 

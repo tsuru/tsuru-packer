29c88dd1f61f1428ba86b0b10272469c6d6ae981
Francisco Souza <fss@corp.globo.com>
2015-12-01 07:43:49 -0200

    setup: install packer 0.8.6

--- setup.bash
+++ setup.bash
@@ -14,8 +14,8 @@ if [[ ! -f /var/cache/virtualbox_4.3_installed ]]; then
     sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-4.3.10-93012.vbox-extpack
 
     sudo apt-get install -y --force-yes unzip git
-    wget https://dl.bintray.com/mitchellh/packer/packer_0.8.2_linux_amd64.zip
-    unzip packer_0.8.2_linux_amd64.zip
+    wget https://releases.hashicorp.com/packer/0.8.6/packer_0.8.6_linux_amd64.zip
+    unzip packer_0.8.6_linux_amd64.zip
     sudo cp packer* /usr/local/bin/
     sudo touch /var/cache/virtualbox_4.3_installed
 fi
